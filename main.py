d955c8a8766165f2027f8c9812fb9c0130e137f0
zeesshhh0 <zishan@videosdk.live>
2026-06-27 16:02:08 +0530

    updated the main.py file to use inference providers

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-from videosdk.agents import Agent, AgentSession, Pipeline, JobContext, RoomOptions, WorkerJob
-from videosdk.plugins.openai import OpenAILLM
-from videosdk.plugins.deepgram import DeepgramSTT
-from videosdk.plugins.elevenlabs import ElevenLabsTTS
+from videosdk.agents import Agent, AgentSession,Pipeline, JobContext, RoomOptions, WorkerJob
 from videosdk.plugins.silero import SileroVAD
+from videosdk.agents.inference import LLM, TTS, STT
+
 
 from videosdk.agents.inference import TurnV2
 
@@ -12,9 +11,12 @@ class MyVoiceAgent(Agent):
             instructions=(
                 "You are VideoSDK's Voice Agent. You are a helpful voice assistant "
                 "that can answer questions about the weather. "
-                "IMPORTANT: don't generate responses above 2 lines."
+                "when ever user asks for the joke tell them this joke : 'Why did the dinosaur cross the road? Because chickens didn't exist yet'"
             ),
+            use_base_instructions=True
         )
+        
+
 
     async def on_enter(self) -> None:
         await self.session.say("Hello, how can I help you today?")  #type: ignore
@@ -27,24 +29,37 @@ async def start_session(context: JobContext):
     agent = MyVoiceAgent()
     
     # Fastest, lowest latency (default)
-    turn_detector = TurnV2.echo_small()
+    echo_turn_detector = TurnV2.echo_small()
 
     # Higher accuracy
-    # turn_detector = TurnV2.echo_large()
+    # echo_turn_detector = TurnV2.echo_large()
 
-    pipeline = Pipeline(
-        stt=DeepgramSTT(),
-        llm=OpenAILLM(),
-        tts=ElevenLabsTTS(),
+    pipeline =  Pipeline(
+        stt=STT.deepgram(),
+        llm=LLM.google(),
+        tts=TTS.cartesia(),
         vad=SileroVAD(),
-        turn_detector=turn_detector
+        turn_detector=echo_turn_detector,
     )
     
     @pipeline.on("turn_state")
     async def on_turn_state(data: dict):
         # data = {"text": str, "state": "Complete" | "Incomplete" |
         #    "Backchannel" | "Wait" }
-        print(f"[TURN] state={data['state']} text={data['text']!r}")
+        print(f"\n[TURN] state={data['state']} text={data['text']!r}")
+
+    @pipeline.metrics.on("eou")
+    def on_eou_metrics(metrics: dict):
+        """Fired when TurnDetector matches end-of-utterance."""
+        print(
+            f"\n[METRICS] EOU Latency: {metrics.get('eou_latency')}ms | "
+            f"EOU Wait: {metrics.get('eou_wait_ms')}ms"
+        )
+        
+    @pipeline.on("llm")
+    async def on_llm(data: dict):
+        text = data.get("text", "")
+        print(f"\nLLM generated: {text[:100]}...")
 
 
     session = AgentSession(
